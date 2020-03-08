19a06b493499d4c14ec994a06ddc31d8b27dfd25
Owen Jones <owenthefirst93@gmail.com>
2020-03-08 16:32:35 -0400

    added random quote package and implemented it

--- personalwebsite/src/Components/About.jsx
+++ personalwebsite/src/Components/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
     return(
         <div>
             <h1>About me</h1>
-            <p>This is my about page</p>
+            <p>
+                My name is Owen Jones and I'm a full-stack web developer based in Bushwick(Brooklyn,NY). 
+            </p>
         </div>
     )
 }
--- personalwebsite/src/Components/Home.jsx
+++ personalwebsite/src/Components/Home.jsx
@@ -1,18 +1,34 @@
 import React, {Component} from 'react'
+import Quote from 'inspirational-quotes'
 import '../Styles/Home.css'
 
 class Home extends Component {
     constructor(){
         super()
         this.state = {
-            elements: true 
+            elements: true,
+            quote: '' 
         }
     }
+    componentDidMount = () => {
+        // console.log(Quote.getRandomQuote())
+        const randomQuote = Quote.getQuote()
+        console.log('quote', randomQuote)
+        this.setState({
+            quote: randomQuote
+        })
+    }
     render(){
+        const {quote} = this.state
+        console.log(quote)
         return(
             <div className='home-page'>
                 <h1>Owen Jones </h1>
                 <img id='main-pic' src='../IMG_8807.jpeg' alt='Main'></img>
+                <div className='random-quote'>
+                    <p>{quote.text}</p>
+                    <h4>{quote.author}</h4>
+                </div>
             </div>
         )
     }
