7921696c2d8dc57fb801ab932d7ee337f19d9af5
Owen Jones <owenthefirst93@gmail.com>
2020-03-14 11:09:58 -0400

    projects page and skills page

--- personalwebsite/src/Components/Projects.jsx
+++ personalwebsite/src/Components/Projects.jsx
@@ -5,13 +5,45 @@ class Projects extends Component {
     constructor(){
         super()
         this.state = {
-            projectList: []
+            projectList: [
+                {  
+                    name: 'MemeMe',
+                    description: 'A Full-Stack Web Application'
+                },
+                {
+                    name: 'Winstagram',
+                    description: 'A Full stack Web Application'
+                },
+                {
+                    name: 'Tekpack',
+                    description: 'A Full Stack Web Application'
+                },
+                {
+                    name: 'Twitter Clone',
+                    description: 'A HTML and CSS Twitter Clone'
+                }
+            ]
         }
     }
 
     componentDidMount = () => {
-
+        this.renderProjects()
     }
+    
+    renderProjects = () => {
+        const {projectList} = this.state
+        return(
+            projectList.map(proj => {
+                <div id={proj.name}>
+                    <h2>{proj.name}</h2>
+                    <h4>{proj.description}</h4>
+                    <div id='link'>
+                        <a href></a>
+                    </div>
+                </div>
+            })
+        )  
+    } 
     render(){
         return(
             <div className='container'>
--- personalwebsite/src/Components/Skills.jsx
+++ personalwebsite/src/Components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Styles/Skills.css'
 
 const Skills = () => {
     return(
@@ -6,22 +7,28 @@ const Skills = () => {
             <h1 id='heading'> Skills </h1>
             <div className='skill-list'>
                 <div id='js-icon'>
-                    <img src='./pics/JSicon.png'></img>
+                    <img src='./pics/JSicon.png' alt='JS'></img>
                 </div>
                 <div id='html-icon'>
-                    <img src='./pics/HTML5Icon.png'></img>
+                    <img src='./pics/HTML5Icon.png' alt='HTML'></img>
                 </div>       
                 <div id='css-icon'>
-                    <img src='./pics/CSS3Icon.png'></img>
+                    <img src='./pics/CSS3Icon.png' alt='CSS'></img>
                 </div> 
                 <div id='nodejs-icon'>
-                    <img src='./pics/NodejsIcon.png'></img>
+                    <img src='./pics/NodejsIcon.png' alt='NODE'></img>
                 </div>
                 <div id='postgres-icon'>
-                    <img src='./pics/PostgreSQLIcon.png'></img>
+                    <img src='./pics/PostgreSQLIcon.png' alt='PG'></img>
                 </div>
                 <div id='react-icon'>
-                    <img src='./pics/ReactIcon.png'></img>
+                    <img src='./pics/ReactIcon.png' alt='REACT'></img>
+                </div>
+                <div id='github-icon'>
+                    <img src= './pics/GithubIcon.png' alt='GITHUB'></img>
+                </div>
+                <div id='git-icon' alt='GIT'>
+                    <img src='./pics/GitIcon.png'></img>
                 </div>
             </div>
         </div>
