9e53a00f36b8ccc9adf0e7632378c8f3ee8a707d
Owen Jones <owenthefirst93@gmail.com>
2020-07-30 12:34:43 -0400

    changed  project order

--- personalwebsite/src/Components/Projects.jsx
+++ personalwebsite/src/Components/Projects.jsx
@@ -7,8 +7,16 @@ class Projects extends Component {
         super()
         this.state = {
             projectList: [
-                {   
+                {
                     id: 0,
+                    name: 'Choral',
+                    description:'A Full Stack Web Application built with React and Express. Howler JS and the Web Audio API are used for audio processing. Media Player API is used for recording',
+                    mission: 'Choral is a remote musical playground where multiple users can collaborate on a single song. Its our solution for remote musical collaboration',
+                    github: 'https://github.com/PeterFiorentino/Choral',
+                    live: 'https://choral.herokuapp.com/'
+                },
+                {   
+                    id: 1,
                     name: 'MemeMe',
                     description: 'A Full-Stack Web Application built with React and Express. Using React JS for the frontend and Express JS for the backend. A RESTful API that uses a PostgreSql for database management.' ,
                     mission: 'MemeMe is a social media site where users can post their favorite memes. Users can add friends and see the memes their friends post as well.',
@@ -16,7 +24,7 @@ class Projects extends Component {
                     live:''
                 },
                 {   
-                    id: 1,
+                    id: 2,
                     name: 'Winstagram',
                     description: 'A Full stack Web Application built with EJS and Express. Using External Javascript Templates for the frontend and Express JS for the backend. A RESTful API that uses a PostgreSQL for database management.',
                     mission: 'Winstagram is a social media site where users can post their daily wins and views the positive steps other users are taking in their lives. We want a positive platform where people are motivated to be their best',
@@ -24,7 +32,7 @@ class Projects extends Component {
                     live:''
                 },
                 {   
-                    id: 2,
+                    id: 3,
                     name: 'Tekpack',
                     description: 'A Full Stack Web Application built with React and Express. It uses a RESTful API and PostgreSQL for the database.',
                     mission: 'Tekpack is a web application that helps fashion designers easily communicate the specs and model of a clothing piece for their suppliers. We aim to cut down textile waste by lowering the amount of samples needed to make clothing.',
@@ -32,21 +40,13 @@ class Projects extends Component {
                     live:''
                 },
                 {   
-                    id: 3,
+                    id: 4,
                     name: 'Twitter-Clone',
                     description: 'A HTML and CSS Twitter Clone. Built with HTML and CSS.',
                     mission: ' A simple Twitter clone of their landing page.',
                     github: 'https://github.com/ojones311/Pursuit-Core-Web-Twitter-CSS-Assignment',
                     live:''
                 },
-                {
-                    id: 4,
-                    name: 'Choral',
-                    description:'A Full Stack Web Application built with React and Express. Howler JS and the Web Audio API are used for audio processing. Media Player API is used for recording',
-                    mission: 'Choral is a remote musical playground where multiple users can collaborate on a single song. Its our solution for remote musical collaboration',
-                    github: 'https://github.com/PeterFiorentino/Choral',
-                    live: 'https://choral.herokuapp.com/'
-                }
             ]
         }
     }
