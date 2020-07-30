6bfa30ed550b785bf8f54e455e15c429d1cbcd8c
Owen Jones <owenthefirst93@gmail.com>
2020-07-30 12:04:04 -0400

    styling

--- personalwebsite/src/Components/About.jsx
+++ personalwebsite/src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <animated.div style={fade}>
             <div className='bio-container'> 
                 <p id='bio'>
-                    I'm based in Bushwick {'=>'} Brooklyn,NY. I love to snowboard, play soccer, solve puzzles, and cook. There isn't anything that doesn't interest me if it's interesting. So lets make something that people love to use, something they can't picture their life without. Knowing I had that kind of impact and ability to brighten someones day is why I type away.
+                    I'm based in Bushwick {'=>'} Brooklyn,NY. I love to snowboard, play soccer, solve puzzles, and cook. There isn't anything that doesn't interest me if it's interesting. So lets make something that people love to use, something they can't picture their life without.
                 </p>
             </div>
             </animated.div>
